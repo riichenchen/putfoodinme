c2a98767a93a7821eb38c42301563ad54e819408
Chen Chang <riichenchen@gmail.com>
2015-04-04 12:20:31 -0700

    Breaking the database

--- EventAdd.php
+++ EventAdd.php
@@ -1,4 +1,3 @@
-//Hello
 <?php
 $name = $_POST["name"];
 $description = $_POST["description"];
@@ -7,19 +6,29 @@ $lat = $_COOKIE["lat"];
 ?>
 <html>
 <body>
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+<?
+
+//$servername = "localhost";
+$host = "tcp:ex84mmt50m.database.windows.net,1433";
+$username = "putfoodinme@ex84mmt50m";
+$password = "PutF00dInMe";
 $db = "food";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $db);
+//$conn = new mysqli($servername, $username, $password, $db);
+try {
+    $conn = new PDO ( "sqlsrv:server = $host; Database = $db", $username, $password);
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch ( PDOException $e ) {
+    print( "Error connecting to SQL Server." );
+    die(print_r($e));
+}
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+//if ($conn->connect_error) {
+//    die("Connection failed: " . $conn->connect_error);
+//} 
 
 $sql = "INSERT INTO foodinfo (name, description, LocationX, LocationY) VALUES ('$name', '$description', '$long', '$lat')";
 if ($conn->query($sql) === TRUE) {
