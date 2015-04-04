6177f49ce50f825397a50d3cb805c52effb83937
ootks <kshu@caltech.edu>
2015-04-04 15:46:41 -0700

    Test SQL Conversion

--- EventAdd.php
+++ EventAdd.php
@@ -10,7 +10,7 @@ $description = $_POST["description"];
 $host = "tcp:ex84mmt50m.database.windows.net,1433";
 $user = "putfoodinme@ex84mmt50m";
 $pwd = "PutF00dInMe";
-$db = "userdata";
+$db = "food";
 try{
     $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
