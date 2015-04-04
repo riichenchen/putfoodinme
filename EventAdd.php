63334593fc114937e76ef50235146ad1cb678e29
ootks <kshu@caltech.edu>
2015-04-04 16:01:34 -0700

    Starting From Basics

--- EventAdd.php
+++ EventAdd.php
@@ -10,39 +10,12 @@ $description = $_POST["description"];
 $host = "tcp:ex84mmt50m.database.windows.net,1433";
 $user = "putfoodinme@ex84mmt50m";
 $pwd = "PutF00dInMe";
-$db = "userdata";
+$db = "food";
 try{
     $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    // $sql_insert = "INSERT INTO foodinfo (name, description, LocationX, LocationY) 
-    //                VALUES (?,?,?,?)";
-    // $stmt = $conn->prepare($sql_insert);
-    // $stmt->bindValue(1, $name);
-    // $stmt->bindValue(2, $description);
-    // $stmt->bindValue(3, $long);
-    // $stmt->bindValue(4, $lat);
-    // $stmt->execute();\
     echo "still works";
- //    $sql_select = "SELECT * FROM foodinfo";
-	// $stmt = $conn->query($sql_select);
-	// $foods = $stmt->fetchAll(); 
-	// if(count($foods) > 0) {
-	//     echo "<h2>Events:</h2>";
-	//     echo "<table>";
-	//     echo "<tr><th>Name</th>";
-	//     echo "<th>Description</th>";
-	//     echo "<th>Latitude</th>";
-	//     echo "<th>Longitude</th></tr>";
-	//     foreach($foods as $food) {
-	//         echo "<tr><td>".$food['name']."</td>";
-	//         echo "<td>".$food['description']."</td>";
-	//         echo "<td>".$food['LocationX']."</td>";
-	//         echo "<td>".$food['LocationY']."</td></tr>";
-	//     }
-	//     echo "</table>";
-	// } else {
-	//     echo "<h3>No food :(</h3>";
-	// }
+   
 }
 catch(Exception $e){
     die(print_r($e));
