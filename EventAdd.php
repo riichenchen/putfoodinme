e956a402bba8279af5922c791167769e53930939
Chen Chang <riichenchen@gmail.com>
2015-04-04 12:50:39 -0700

    MySQL?

--- EventAdd.php
+++ EventAdd.php
@@ -7,28 +7,18 @@ $lat = $_COOKIE["lat"];
 <html>
 <body>
 <?php
-
-//$servername = "localhost";
-$host = "tcp:ex84mmt50m.database.windows.net,1433";
-$username = "putfoodinme@ex84mmt50m";
-$password = "PutF00dInMe";
+$servername = "us-cdbr-azure-southcentral-e.cloudapp.net";
+$username = "b950be0f214ace";
+$password = "be1d807d ";
 $db = "food";
 
 // Create connection
-//$conn = new mysqli($servername, $username, $password, $db);
-try {
-    $conn = new PDO ( "sqlsrv:server = tcp:ex84mmt50m.database.windows.net,1433; Database = food", $username, $password);
-    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-}
-catch ( PDOException $e ) {
-    print( "Error connecting to SQL Server." );
-    die(print_r($e));
-}
+$conn = new mysqli($servername, $username, $password, $db);
 
 // Check connection
-//if ($conn->connect_error) {
-//    die("Connection failed: " . $conn->connect_error);
-//} 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 
 $sql = "INSERT INTO foodinfo (name, description, LocationX, LocationY) VALUES ('$name', '$description', '$long', '$lat')";
 if ($conn->query($sql) === TRUE) {
