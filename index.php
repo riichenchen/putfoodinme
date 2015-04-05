5a4a423a90cf797d07d51dd8e77cadffb8618c03
Chen Chang <riichenchen@gmail.com>
2015-04-05 03:03:33 -0700

    debug

--- index.php
+++ index.php
@@ -43,8 +43,25 @@
             } else {   // Browser doesn't support Geolocation
                 handleNoGeolocation(false);
             }
-			addMyMarker();
-
+			//addMyMarker();
+			console.log("1");
+			myMarker = new google.maps.Marker({
+                position: new google.maps.LatLng(myLat, myLong),
+                map: map,
+                animation: google.maps.Animation.BOUNCE,
+                draggable: true
+            });
+            console.log("2");
+            google.maps.event.addListener(myMarker, 'click', (function (myMarker) {
+                return function () {
+                    var myInfowindow = new google.maps.InfoWindow();
+                    myInfowindow.setContent("<p> Add food at (" + 
+                        myMarker.position.lat()+", "+myMarker.position.lng()+")</p>");
+                    myInfowindow.open(map, myMarker);
+                }
+            })(myMarker));
+			console.log("3");
+			
 			refreshFood();
         }
 		
